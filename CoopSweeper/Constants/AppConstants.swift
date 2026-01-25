9d24796ed82251ba224cedcf23e186dab4d87af2
Mikheil Muchaidze <mikheil.muchaidze@gmail.com>
2026-01-25 12:49:36 +0400

    Added some default launch screen which will be changed(background and app icon)

--- CoopSweeper/Constants/AppConstants.swift
+++ CoopSweeper/Constants/AppConstants.swift
@@ -32,29 +32,4 @@ enum AppConstants {
         }
         .ignoresSafeArea()
     }
-
-//    struct BackgroundGradientView: View {
-//        var body: some View {
-//            LinearGradient(
-//                colors: [
-//                    Color(red: 0.15, green: 0.25, blue: 0.30),
-//                    Color(red: 0.08, green: 0.15, blue: 0.20),
-//                    Color(red: 0.05, green: 0.10, blue: 0.15)
-//                ],
-//                startPoint: .top,
-//                endPoint: .bottom
-//            )
-//            .overlay(
-//                RadialGradient(
-//                    colors: [
-//                        Color(red: 0.2, green: 0.4, blue: 0.5).opacity(0.3),
-//                        Color.clear
-//                    ],
-//                    center: .topTrailing,
-//                    startRadius: 100,
-//                    endRadius: 400
-//                )
-//            )
-//        }
-//    }
 }
